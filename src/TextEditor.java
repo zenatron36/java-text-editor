3f61358dd159775be0683f08add585254a6be62d
Phil <gadgetsphilop36@gmail.com>
2023-01-17 20:08:38 -0500

    added a comment

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
-
+//Now this is going to be epic.
 public class TextEditor extends JFrame {
 
     private JTextArea textArea;
